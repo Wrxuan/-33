444e645e01ffad1d331edc466f8d4132427dfc3a
Wrxuan <2450341686@qq.com>
2022-09-06 16:13:10 +0800

    feat:postcss配置完毕

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>黑马头条</div>
+  <div>
+    黑马头条
+    <span class="toutiao toutiao-gengduo">11</span>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <div class="box"></div>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style scoped>
+  .box{
+    width: 2rem;
+    height: 2rem;
+    background-color: red;
+  }
+</style>
