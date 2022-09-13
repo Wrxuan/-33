dbef33a02a905aeb826abbc0d7a1b7a318138a0f
Wrxuan <2450341686@qq.com>
2022-09-13 17:34:32 +0800

    reat:首页功能完成一半

--- src/views/Home/components/ArticleList.vue
+++ src/views/Home/components/ArticleList.vue
@@ -1,6 +1,23 @@
 <template>
-  <div>
-    <article-item v-for="item in articles" :key="item.art_id" :article="item"></article-item>
+  <div class="article">
+    <van-pull-refresh v-model="refreshLoading" @refresh="getNextPageArticle">
+      <van-list
+        v-model="loading"
+        @load="getNextPageArticle"
+        offset="100"
+        :immediate-check="false"
+        :finished="finished"
+        :error.sync="error"
+        error-text="请求失败，点击重新加载"
+        finished-text="没有更多文章了~"
+      >
+        <article-item
+          v-for="item in articles"
+          :key="item.art_id"
+          :article="item"
+        ></article-item>
+      </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 
@@ -14,7 +31,11 @@ export default {
   },
   data() {
     return {
-      articles: []
+      articles: [],
+      loading: false,
+      finished: false,
+      error: false,
+      refreshLoading: false
     }
   },
   created() {
@@ -26,6 +47,7 @@ export default {
         const { data } = await getArticles(this.id, +new Date())
         this.articles = data.data.results
         console.log(data)
+        this.preTimestamp = data.data.pre_timestamp
       } catch (error) {
         // js的错误，.507原封不动上抛. 400 上抛中文
         const status = error.response?.status
@@ -37,9 +59,35 @@ export default {
           }
         }
       }
+    },
+    async getNextPageArticle() {
+      try {
+        const { data } = await getArticles(this.id, this.preTimestamp)
+        if (!data.data.pre_timestamp) {
+          this.finished = true
+        }
+        // 如果是下滑加载，将数据放在最后
+        // 如果是下拉刷新，将数据放在最前
+        if (this.refreshLoading) {
+          this.articles.unshift(...data.data.results)
+        } else {
+          this.articles.push(...data.data.results)
+        }
+        this.preTimestamp = data.data.pre_timestamp
+      } catch (error) {
+        this.error = true
+      } finally {
+        this.loading = false
+        this.refreshLoading = false
+      }
     }
   }
 }
 </script>
 
-<style></style>
+<style scoped lang="less">
+.article {
+  height: calc(100vh - 92px - 83px - 100px);
+  overflow: auto;
+}
+</style>
